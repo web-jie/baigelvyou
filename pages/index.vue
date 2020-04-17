21b1ace8a83450eae1b5b277ec6b493504d91b97
web-jie <864214989@qq.com>
2020-04-17 18:10:08 +0800

    设置主页轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>首页</div>
+  <div>
+    <el-carousel height="700px" >
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div class="lun"
+          :style="`background: url(${$axios.defaults.baseURL + item.url}) center center no-repeat;`"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      // 存放轮播图的位置
+      banners: []
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      const { data } = res.data;
+      
+      this.banners = data;
+    });
+  }
+};
 </script>
 
-<style>
-
+<style scoped lang="less" >
+.lun{
+  height: 700px;
+}
+/deep/ .el-carousel{
+  min-width: 1000px;
+}
 </style>
