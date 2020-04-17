7b3e2c7c2f9dfc33df3995c315dc48237483aa6c
web-jie <864214989@qq.com>
2020-04-17 09:36:03 +0800

    第一次提交

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- 头部组件 -->
+    <Header/>
+    <!-- nuxt就是router-view -->
+    <nuxt />
+
+  </div>
+</template>
+
+<script>
+// 导入头部组件
+import Header from "@/components/header"
+export default {
+   // 注册组件
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  margin:0; 
+  padding:0;
+}
+ul, li, ol{
+  list-style:none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+}
+a:hover{
+
+}
+em,i{
+  font-style: normal;
+}
+</style>
