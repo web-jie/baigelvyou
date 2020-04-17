2a704782678669f3482663eddb9aeefa88c86dc5
web-jie <864214989@qq.com>
2020-04-17 10:03:05 +0800

    添加底部导航栏

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,21 @@
     <Header/>
     <!-- nuxt就是router-view -->
     <nuxt />
-
+<!-- 页脚组件 -->
+    <Footer/>
   </div>
 </template>
 
 <script>
 // 导入头部组件
 import Header from "@/components/header"
+// 导入页脚组件
+import Footer from "@/components/footer";
 export default {
    // 注册组件
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
