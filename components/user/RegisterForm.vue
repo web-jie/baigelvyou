f2e7dfcca196d7ce42074715f201511b9e49bd01
web-jie <864214989@qq.com>
2020-04-20 11:23:23 +0800

    創建手机验证码

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -1,10 +1,10 @@
 <template>
   <el-form :model="form" ref="form" :rules="rules" class="form">
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="username">
       <el-input v-model="form.username" placeholder="用户名手机"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="captcha">
       <el-input v-model="form.captcha" placeholder="验证码">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
@@ -12,16 +12,16 @@
       </el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="nickname">
       <el-input v-model="form.nickname" placeholder="你的名字"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="password">
       <el-input v-model="form.password" placeholder="密码" type="password"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
-      <el-input v-model="checkPassword" placeholder="确认密码" type="password"></el-input>
+    <el-form-item class="form-item" prop="checkPassword">
+      <el-input v-model="form.checkPassword" placeholder="确认密码" type="password"></el-input>
     </el-form-item>
 
     <el-button class="submit" type="primary" @click="handleRegSubmit">注册</el-button>
@@ -31,6 +31,15 @@
 <script>
 export default {
   data() {
+    const validatePassword = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.form.password) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
       // 表单数据
       form: {
@@ -42,37 +51,37 @@ export default {
       },
       // 表单规则
       rules: {
-        username: {
-          required: true,
-          message: "请输入手机号码",
-          trigger: "blur"
-        },
-        nickname: {
-          required: true,
-          message: "请输入昵称",
-          trigger: "blur"
-        },
-        captcha: {
-          required: true,
-          message: "请输入验证码",
-          trigger: "blur"
-        },
-        password: {
-          required: true,
-          message: "请输入密码",
-          trigger: "blur"
-        },
-        checkPassword: {
-          required: true,
-          message: "两次输入密码不一致",
-          trigger: "blur"
-        },
+        username: [
+          { required: true, message: "请输入手机号码", trigger: "blur" }
+        ],
+        nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+        captcha: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+
+        checkPassword: [{ validator: validatePassword, trigger: "blur" }]
       }
     };
   },
   methods: {
     // 发送验证码
-    handleSendCaptcha() {},
+    handleSendCaptcha() {
+      if (this.form.username === "") {
+        // 主动的触发表单某个属性字段的校验，并且会主动发生错误提示
+        this.$refs.form.validateField("username");
+        return;
+      }
+      this.$axios({
+        url: "/captchas",
+        method: "POST",
+        // 电话号码
+        data: {
+          tel: this.form.username
+        }
+      }).then(res => {
+        const { code } = res.data;
+        this.$message.success("验证码为" + code);
+      });
+    },
 
     // 注册
     handleRegSubmit() {
