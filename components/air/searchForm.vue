1e19b37ee2c07a5b3c9da081b57d9315d74bdc80
web-jie <864214989@qq.com>
2020-04-21 22:27:30 +0800

    交换地址

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -13,36 +13,31 @@
       </span>
     </el-row>
 
-    <!-- 机票搜索的表单 -->
-    <el-form class="search-form-content" :rules="rules" ref="form" label-width="80px">
-      <!-- 出发城市的输入框 -->
+    <el-form class="search-form-content" ref="form" :model="form" :rules="rules" label-width="80px">
       <el-form-item label="出发城市" prop="departCity">
-        <!-- fetch-suggestions：获取搜索建议，它的功能就是根据当前输入的关键字，发起请求，把请求的结果显示下拉列表中 -->
-        <!-- fetch-suggestions比较类似input事件，有监听作用，不过他可以把数据展示在下拉列表 -->
-        <!-- @select: 选中下拉列表某一项的时候触发的事件，通过参数获取到当前选中的那个选项 -->
+        <!-- fetch-suggestions 返回输入建议的方法 -->
+        <!-- select 点击选中建议项时触发 -->
         <el-autocomplete
           :fetch-suggestions="queryDepartSearch"
           placeholder="请搜索出发城市"
           @select="handleDepartSelect"
           class="el-autocomplete"
-          v-model="form.destCity"
+          v-model="form.departCity"
           @blur="handlDepartBlur"
         ></el-autocomplete>
       </el-form-item>
-      <!-- 到达城市的输入框 -->
       <el-form-item label="到达城市" prop="destCity">
         <el-autocomplete
           :fetch-suggestions="queryDestSearch"
           placeholder="请搜索到达城市"
           @select="handleDestSelect"
           class="el-autocomplete"
+          v-model="form.destCity"
           @blur="handlDestBlur"
         ></el-autocomplete>
       </el-form-item>
-      <!-- 出发时间 -->
       <el-form-item label="出发时间" prop="departDate">
         <!-- change 用户确认选择日期时触发 -->
-        <!-- value-format 设置时间的格式 -->
         <el-date-picker
           type="date"
           placeholder="请选择日期"
@@ -72,9 +67,9 @@ export default {
         { icon: "iconfont iconshuangxiang", name: "往返" }
       ],
       currentTab: 0,
-      // 表单的数据
+      // 表单数据
       form: {
-        departCity: "", // 出发城市
+        departCity: "", //出发的城市
         departCode: "", // 出发城市的字母代码
         destCity: "", //到达城市
         destCode: "", // 到达城市的字母代码
@@ -82,7 +77,7 @@ export default {
       },
       // 出发城市的下拉列表数据
       departCities: [],
-      // 出发城市的下拉列表数据
+      // 目的城市的下拉列表数据
       destCities: [],
       // 日期禁用选项
       pickerOptions: {
@@ -105,7 +100,8 @@ export default {
     };
   },
   methods: {
-    gitCities(value){
+    // 封装请求城市的方法
+    getCities(value) {
       // 请求和value相关的城市
       return this.$axios({
         url: "/airs/city",
@@ -120,27 +116,33 @@ export default {
           v.value = v.name.replace("市", "");
           return v;
         });
-      })
+
+        return newData;
+      });
     },
     // tab切换时触发
     handleSearchTab(item, index) {},
+
     // 出发城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
-    // cb接收的参数有个固定的格式，参数必须是一个数组，并且数组是由对象组成的，还有对象必须要有value属性
     queryDepartSearch(value, cb) {
-      // // 如果value值空的，就不需要请求
+      // 如果value没有值就不用去请求了
+      //cb接收的参数有个固定的格式，参数必须是一个数组，并且数组是由对象组成的，还有对象必须要有value属性
       if (!value) {
+        // 禁止输入框的值是空的时候显示下拉框
+        cb([]);
+        // 如果输入框的值是空的话把之前的城市列表删除掉
+        this.departCities = [];
         return;
       }
-       // 监听输入框有值的时候重新验证表单，可以消除掉红的报错信息
-            this.$refs.form.validateField("departCity");
-
-this.getCities(value).then(res=>{
+      // 监听输入框有值的时候重新验证表单，可以消除掉红的报错信息
+      this.$refs.form.validateField("departCity");
+      this.getCities(value).then(newData => {
         // 保存到data中，给blur事件使用, 失去焦点时候选中第一个
         this.departCities = newData;
-        // cb是要请求成功之后才调用，因为在这里才可以拿到城市的数据
         cb(newData);
       });
+      //  cb是要请求成功之后才调用，因为在这里才可以拿到城市的数据
     },
     // 出发城市失去焦点时候默认选中第一个
     handlDepartBlur() {
@@ -149,34 +151,23 @@ this.getCities(value).then(res=>{
         this.form.departCode = this.departCities[0].sort;
       }
     },
+
     // 目标城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
+
     queryDestSearch(value, cb) {
-      // // 如果value值空的，就不需要请求
       if (!value) {
+        // 禁止输入框的值是空的时候显示下拉框
+        cb([]);
+        // 如果输入框的值是空的话把之前的城市列表删除掉
+        this.destCities = [];
         return;
       }
       // 监听输入框有值的时候重新验证表单，可以消除掉红的报错信息
-            this.$refs.form.validateField("destCity");
-
-      // // 请求和value相关的城市
-      // this.$axios({
-      //   url: "/airs/city",
-      //   params: {
-      //     name: value
-      //   }
-      // }).then(res => {
-      //   // data是城市的数组
-      //   const { data } = res.data;
-      //   // data的属性没有value属性，需要转换下
-      //   const newData = data.map(v => {
-      //     v.value = v.name.replace("市", "");
-      //     return v;
-      //   });
-      this.getCities(value).then(res=>{
+      this.$refs.form.validateField("destCity");
+      this.getCities(value).then(newData => {
         // 保存到data中，给blur事件使用, 失去焦点时候选中第一个
         this.destCities = newData;
-        // cb是要请求成功之后才调用，因为在这里才可以拿到城市的数据
         cb(newData);
       });
     },
@@ -187,31 +178,43 @@ this.getCities(value).then(res=>{
         this.form.destCode = this.destCities[0].sort;
       }
     },
+
     // 出发城市下拉选择时触发
     handleDepartSelect(item) {
       this.form.departCode = item.sort;
     },
+
     // 目标城市下拉选择时触发
     handleDestSelect(item) {
       this.form.destCode = item.sort;
     },
+
     // 确认选择日期时触发
     handleDate(value) {},
+
     // 触发和目标城市切换时触发
-    handleReverse() {},
+    handleReverse() {
+      // 交叉更换值
+      const {departCity,departCode,destCity,destCode} = this.form
+      this.form.departCity = destCity
+      this.form.departCode = destCode
+
+      this.form.destCity = departCity
+      this.form.destCode = departCode
+    },
+
     // 提交表单是触发
     handleSubmit() {
-       // 表单验证
-            this.$refs.form.validate(valid => {
-                if(valid){
-                     // 路由跳转，path指定的路径，query属性指定的问号后面的参数
-                    // 如果是动态参数就使用params
-                    this.$router.push({
-                        path: "/air/flights",
-                        query: this.form
-                    })
-                }
-            })
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          // 路由跳转，path指定的路径，query属性指定的问号后面的参数
+          // 如果是动态参数就使用params
+          this.$router.push({
+            path: "/air/flights",
+            query: this.form
+          });
+        }
+      });
     }
   },
   mounted() {}
@@ -226,6 +229,7 @@ this.getCities(value).then(res=>{
   height: 350px;
   box-sizing: border-box;
 }
+
 .search-tab {
   span {
     display: block;
@@ -237,29 +241,36 @@ this.getCities(value).then(res=>{
     border-top: 3px #eee solid;
     background: #eee;
   }
+
   .active {
     border-top-color: orange;
     background: #fff;
   }
+
   i {
     margin-right: 5px;
     font-size: 18px;
+
     &:first-child {
       font-size: 16px;
     }
   }
 }
+
 .search-form-content {
   padding: 15px 50px 15px 15px;
   position: relative;
+
   .el-autocomplete {
     width: 100%;
   }
 }
+
 .reverse {
   position: absolute;
   top: 35px;
   right: 15px;
+
   &:after,
   &:before {
     display: block;
@@ -270,12 +281,15 @@ this.getCities(value).then(res=>{
     height: 1px;
     background: #ccc;
   }
+
   &:after {
     top: 0;
   }
+
   &:before {
     top: 60px;
   }
+
   span {
     display: block;
     position: absolute;
@@ -290,6 +304,7 @@ this.getCities(value).then(res=>{
     text-align: center;
     border-radius: 2px;
     cursor: pointer;
+
     &:after,
     &:before {
       display: block;
@@ -300,9 +315,11 @@ this.getCities(value).then(res=>{
       height: 20px;
       background: #ccc;
     }
+
     &:after {
       top: -20px;
     }
+
     &:before {
       top: 20px;
     }
