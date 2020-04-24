dd35e214dd9ce6898289b4bfc4423435f3eaf5b3
web-jie <864214989@qq.com>
2020-04-24 21:40:48 +0800

    删除用户数据

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -5,7 +5,7 @@
       <el-form class="member-info">
         <div class="member-info-item" v-for="(item,index) in form.users" :key="index">
           <el-form-item label="乘机人类型">
-            <el-input placeholder="姓名" class="input-with-select">
+            <el-input placeholder="姓名" class="input-with-select" v-model="item.username">
               <el-select slot="prepend" value="1" placeholder="请选择">
                 <el-option label="成人" value="1"></el-option>
               </el-select>
@@ -13,14 +13,14 @@
           </el-form-item>
 
           <el-form-item label="证件类型">
-            <el-input placeholder="证件号码" class="input-with-select">
+            <el-input placeholder="证件号码" class="input-with-select" v-model="item.id">
               <el-select slot="prepend" value="1" placeholder="请选择">
                 <el-option label="身份证" value="1" :checked="true"></el-option>
               </el-select>
             </el-input>
           </el-form-item>
-
-          <span class="delete-user" @click="handleDeleteUser()">-</span>
+          <!-- 删除用户 -->
+          <span class="delete-user" @click="handleDeleteUser(index)">-</span>
         </div>
       </el-form>
 
@@ -83,12 +83,15 @@ export default {
     handleAddUsers() {
       this.form.users.push({
         username: "",
-        id: ''
-      })
+        id: ""
+      });
     },
 
     // 移除乘机人
-    handleDeleteUser() {},
+    handleDeleteUser(index) {
+      // 根据点击的index移除乘机人
+      this.form.users.splice(index, 1);
+    },
 
     // 发送手机验证码
     handleSendCaptcha() {},
