ab25e728c5c0c31bc6c354076f311ff6a3078aea
web-jie <864214989@qq.com>
2020-04-27 22:27:30 +0800

    根据关键字来查询路线

--- pages/map.vue
+++ pages/map.vue
@@ -5,7 +5,7 @@
       src="https://webapi.amap.com/maps?v=1.4.15&key=1677f9f27478834cd981816588a1cee2"
     ></script>
     <h2>高德地图</h2>
-    <el-row type="flex" align="center" :gutter="32">
+    <el-row type="flex" align="center" :gutter="80">
       <!-- 下面的 container中千万不要加内容  -->
       <el-col :span="12">
         <div id="container"></div>
@@ -20,13 +20,13 @@
         <div class="form">
           <el-form>
             <el-form-item>
-              <el-input placeholder="起点位置"></el-input>
+              <el-input placeholder="起点位置" v-model="start"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-input placeholder="终点位置"></el-input>
+              <el-input placeholder="终点位置" v-model="end"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" style="width: 100%">查询</el-button>
+              <el-button type="primary" style="width: 100%" @click="handleSearch">查询</el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -43,9 +43,11 @@ export default {
   data() {
     return {
       resize: true,
-      map: {},
       trafficLayer: {},
-      activeName: "first"
+      activeName: "first", // 当前驾车还是公交或者步行
+      map: "", // 地图对象
+      start: "", // 起点位置
+      end: "" // 终点位置
     };
   },
   mounted() {
@@ -81,6 +83,27 @@ export default {
       }
       this.resize = !this.resize;
       console.log(this.resize);
+    },
+    // 查询驾车路线
+    handleDriving() {
+      AMap.plugin("AMap.Driving", () => {
+        var driving = new AMap.Driving({
+          map: this.map
+        });
+
+        var points = [
+          { keyword: this.start, city: "广州" },
+          { keyword: this.end, city: "广州" }
+        ];
+
+        driving.search(points, function(status, result) {
+          // 未出错时，result即是对应的路线规划方案
+        });
+      });
+    },
+    // 点击查询按钮，开始查询路线
+    handleSearch() {
+      this.handleDriving();
     }
   }
 };
